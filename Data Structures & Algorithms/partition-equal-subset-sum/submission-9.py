a8189c056613d9ff19131011430a4927c9824058
Mamonaka <50657299+BorueiChen@users.noreply.github.com>
2026-04-05 19:15:52 +0800

    Add: partition-equal-subset-sum - submission-9

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-9.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        ans = False
+        length = len(nums)
+        def dfs(idx, target):
+            nonlocal ans
+            if ans == True:
+                return
+            if target == 0:
+                ans = True
+                return
+            if target < 0 or idx == length:
+                return
+            
+            dfs(idx+1, target - nums[idx])
+            dfs(idx+1, target)
+            return
+        
+        if sum(nums) % 2 == 0:
+            dfs(0, sum(nums)/2)
+        return ans
