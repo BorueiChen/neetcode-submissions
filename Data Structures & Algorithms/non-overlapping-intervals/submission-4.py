58e2aebc3ec0ee3848d948d3796e52760e3ccbbc
Mamonaka <50657299+BorueiChen@users.noreply.github.com>
2026-04-08 20:02:03 +0800

    Add: non-overlapping-intervals - submission-4

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: x[1])
+        # (1,2), (2,4), (1,4)
+        cur_s, cur_e = intervals[0]
+        remove = 0
+        for idx in range(1, len(intervals)):
+            s, e = intervals[idx]
+            print(f"cur s:{cur_s}, e:{cur_e}// s:{s}, e:{e}")
+            if not (s <= cur_s and e <= cur_s) and not (s >= cur_s and e >= cur_s) and \
+            not (cur_s == s and cur_e == e):
+                cur_s, cur_e = s, e
+            else:
+                remove += 1
+        return remove
