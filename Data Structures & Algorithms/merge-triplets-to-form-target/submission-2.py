b1eb97cc8c81830e5dd1fea7d6552ac63e90a1ec
Mamonaka <50657299+BorueiChen@users.noreply.github.com>
2026-04-21 21:39:21 +0800

    Add: merge-triplets-to-form-target - submission-2

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        ans = False
+        found = set()
+        candiate = {} # target[0]: [idx in triplet], ... 
+        for c_idx, triplet in enumerate(triplets):
+            for idx, num in enumerate(triplet):
+                if num == target[idx]:
+                    if num not in candiate:
+                        candiate[num] = []
+                    candiate[num].append(c_idx)
+
+        result = []
+        for c1 in candiate.get(target[0], []):
+            for c2 in candiate.get(target[1], []):
+                for c3 in candiate.get(target[2], []):
+                    result.append([max(triplets[c1][0],  triplets[c2][0], triplets[c3][0]), 
+                                max(triplets[c1][1],  triplets[c2][1], triplets[c3][1]), 
+                                max(triplets[c1][2],  triplets[c2][2], triplets[c3][2])])
+        
+
+        return target in result
