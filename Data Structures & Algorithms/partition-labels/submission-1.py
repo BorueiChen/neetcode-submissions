55fb36b71410b380cdd0621ed6e62187d83bfa31
Mamonaka <50657299+BorueiChen@users.noreply.github.com>
2026-04-23 19:32:31 +0800

    Add: partition-labels - submission-1

---
+++ Data Structures & Algorithms/partition-labels/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        max_index = {}
+        n = len(s)
+        for idx in range(n):
+            max_index[s[idx]] = idx
+        
+        ans = []
+        length = 1
+        end = -1
+        for idx in range(n):
+            if idx == end or idx == n - 1:
+                ans.append(length)
+                length = 1
+            elif idx > end:
+                end = max_index[s[idx]]
+                if idx == end:
+                    ans.append(length)
+                    length = 1
+            else:
+                length += 1
+                end = max(end, max_index[s[idx]])
+        return ans
+            
