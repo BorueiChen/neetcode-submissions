21adf4e7b2547d8261b263f83596c17515ea42c4
Mamonaka <50657299+BorueiChen@users.noreply.github.com>
2026-04-26 10:04:32 +0800

    Add: valid-parenthesis-string - submission-3

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-3.py
@@ -0,0 +1,36 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        left, right = 0, 0
+        star_left, star_right = 0, 0
+
+        n = len(s)
+        for idx in range(n):
+            s_left = s[idx]
+            s_right = s[n - 1 - idx]
+
+            # left
+            if s_left == ')':
+                if left:
+                    left -= 1
+                elif star_left:
+                    star_left -= 1
+                else:
+                    return False
+            elif s_left == '(':
+                left += 1
+            else:
+                star_left += 1
+
+            # right
+            if s_right == '(':
+                if right:
+                    right -= 1
+                elif star_right:
+                    star_right -= 1
+                else:
+                    return False
+            elif s_right == ')':
+                right += 1
+            else:
+                star_right += 1    
+        return True        
